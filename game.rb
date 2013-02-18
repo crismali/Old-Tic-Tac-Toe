c65f42c3985ca0f0c8d62d12fc5ee41947973298
Michael Crismali <michael.crismali@gmail.com>
2013-02-18 15:15:38 -0600

    put board.rb in game.rb

--- game.rb
+++ game.rb
@@ -1,9 +1,9 @@
 class Game
 
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :player_1, :player_2, :board_array
 
   def initialize
-    @board = Board.new
+    @board_array = { [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] }
     @player_1 = HumanPlayer.new
     #calls a method that intros the game
     introduce_game
@@ -77,6 +77,20 @@ class Game
   def vertical_win?(letter)
 
 
+  end
+
+   def draw_board
+    system ('clear')
+
+    puts "     |     |      "
+    puts "  #{ @board_array[0] }  |  #{ @board_array[1] }  |  #{ @board_array[2] }"
+    puts "_____|_____|_____"
+    puts "     |     |    "
+    puts "  #{ @board_array[3] }  |  #{ @board_array[4] }  |  #{ @board_array[5] }"
+    puts "_____|_____|_____"
+    puts "     |     |    "
+    puts "  #{ @board_array[6] }  |  #{ @board_array[7] }  |  #{ @board_array[8] }"
+    puts "     |     |    "
   end
 
 end
