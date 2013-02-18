227328df47f99e6f108c4596b3ea73d1904eb469
Michael Crismali <michael.crismali@gmail.com>
2013-02-18 17:01:26 -0600

    refactored/cleaned up who_won logic
    also moved victory checks

--- game.rb
+++ game.rb
@@ -1,94 +1,37 @@
 class Game
 
   include Setup
+  include VictoryChecks
 
-  attr_accessor :player_1, :player_2, :board_array
+  attr_accessor :player_1, :player_2, :board_array, :who_won
 
   def initialize
     @board_array = { [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] }
     @player_1 = HumanPlayer.new
-    #calls a method that intros the game
+    @player_2 = nil
+    @who_won = false
+
     introduce_game
-    #asks if it's human vs. human or human vs. cpu
+
     play_against_friend_or_cpu?
+
+    play_a_round
+
   end
 
   def play_a_round
-    # did_anyone_win?
-    #whatever needs to happen to make a round happen
-    return "Cat's game!" if cats_game?
+    #put players in array so .each can be used and the repeated code can be put in a block
     @player_1.mark_the_board(@board_array)
-    @player_2.mark_the_board(@board_array)
+
     did_anyone_win?
-  end
 
-  def cats_game?
-    @board_array.uniq.size == 2
-  end
+    return "Cat's game!" if cats_game?
 
-  def did_anyone_win?
+    @player_2.mark_the_board(@board_array)
 
-    who_won = false
-    who_won = diagonal_win?
-    who_won = horizontal_win? unless who_won
-    who_won = vertical_win? unless who_won
-    return who_won
+    did_anyone_win?
 
-  end
-
-  def diagonal_win?
-
-    diagonal_1 = @board_array.values_at(0,4,8)
-    diagonal_2 = @board_array.values_at(2,4,6)
-
-    if all_same_elements?(diagonal_1)
-      return diagonal_1.first
-    elsif all_same_elements?(diagonal_2)
-      return diagonal_2.first
-    else
-      return false
-    end
-
-  end
-
-  def horizontal_win?
-
-    row_1 = @board_array.take(3)
-    row_2 = @board_array.values_at(3,4,5)
-    row_3 = @board_array.values_at(6,7,8)
-
-    if all_same_elements?(row_1)
-      return row_1.first
-    elsif all_same_elements?(row_2)
-      return row_2.first
-    elsif all_same_elements?(row_3)
-      return row_3.first
-    else
-      return false
-    end
-
-  end
-
-  def vertical_win?
-
-    column_1 = @board_array.values_at(0,3,6)
-    column_2 = @board_array.values_at(1,4,7)
-    column_3 = @board_array.values_at(2,5,8)
-
-    if all_same_elements?(column_1)
-      return column_1.first
-    elsif all_same_elements?(column_2)
-      return column_2.first
-    elsif all_same_elements?(column_3)
-      return column_3.first
-    else
-      return false
-    end
-
-  end
-
-  def all_same_elements?(array)
-    array.uniq.size == 1
+    return "Cat's game!" if cats_game?
   end
 
   def draw_board
