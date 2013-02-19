5dbf82750c51328735f2d9bdc47e32524d7380ae
Michael Crismali <michael.crismali@gmail.com>
2013-02-19 08:34:02 -0600

    added play again logic

--- play.rb
+++ play.rb
@@ -8,3 +8,17 @@ require './game.rb'
 
 tictactoe = Game.new
 
+dont_want_to_play_again = false
+
+until dont_want_to_play_again
+  puts "Would you like to play again? (1 for yes, 2 for no)"
+  input = gets.chomp
+  if input == '1'
+    tictactoe = Game.new
+  elsif input == '2'
+    dont_want_to_play_again = true
+    puts "Thanks for playing!"
+  else
+    "Please enter a 1 (to play again) or a 2 (to quit playing)"
+  end
+end
