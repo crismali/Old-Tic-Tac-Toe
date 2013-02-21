8177d77595f9eafbca6797041c6721bdf7cbc405
Michael Crismali <michael.crismali@gmail.com>
2013-02-21 09:55:13 -0600

    cleaned up cpu player, added complete for win method

--- computer_player.rb
+++ computer_player.rb
@@ -18,7 +18,11 @@ class ComputerPlayer
 
     cpu_choice = false
 
-    cpu_choice = block_opponent?(board_array)
+    cpu_choice = complete_for_win?(get_diagonals(board_array), get_rows(board_array), get_columns(board_array))
+
+    cpu_choice = block_opponent?(board_array) unless cpu_choice
+
+    cpu_choice = stop_corner_strategy?(board_array) unless cpu_choice
 
     cpu_choice = play_strategy(board_array) unless cpu_choice
 
@@ -40,12 +44,16 @@ class ComputerPlayer
 
   def if_should_block_logic(*lines)
 
-    if section_should_be_blocked?(lines[1]) && any_spaces_available?(lines[1]).first
-      return any_spaces_available?(lines[1]).first
-    elsif section_should_be_blocked?(lines[2]) && any_spaces_available?(lines[2]).first
-      return any_spaces_available?(lines[2]).first
-    elsif lines[3].present? && section_should_be_blocked?(lines[3]) && any_spaces_available?(lines[3]).first
-      return any_spaces_available?(lines[3]).first
+    available_in_line_1 = any_spaces_available?(lines[1]).first
+    available_in_line_2 = any_spaces_available?(lines[2]).first
+    available_in_line_3 = any_spaces_available?(lines[3]).first
+
+    if section_should_be_blocked?(lines[1]) && available_in_line_1
+      return available_in_line_1
+    elsif section_should_be_blocked?(lines[2]) && available_in_line_2
+      return available_in_line_2
+    elsif lines[3].present? && section_should_be_blocked?(lines[3]) && available_in_line_3
+      return available_in_line_3
     else
       return false
     end
@@ -56,12 +64,33 @@ class ComputerPlayer
     array.uniq.size == 2
   end
 
-  def any_spaces_available?(array)
+  def complete_for_win?(*array_of_lines)
+
+    array_of_lines.flatten(1).each do |line|
+
+      if section_should_be_blocked?(line)
+
+        if line.uniq[0].is_a? Integer && line.uniq[1] == 'O'
+          return any_spaces_available?(line)
+        elsif line.uniq[1].is_a? Integer && line.uniq[0] == 'O'
+          return any_spaces_available?(line)
+        end
+
+      end
+
+    end
+
+    return false
+
+  end
+
+
+  def any_spaces_available?(line)
 
     available_spaces = Array.new
 
-    array.each do |space|
-        available_spaces << array.index(space) if space.is_a? Integer
+    line.each do |space|
+        available_spaces << line.index(space) if space.is_a? Integer
     end
 
     available_spaces << false if available_spaces.empty?
@@ -70,10 +99,16 @@ class ComputerPlayer
 
   end
 
+  def stop_corner_strategy?(board_array)
+
+
+
+  end
+
   def play_strategy(board_array)
 
     board_array.uniq.select! { |element| element.is_a? Integer}
-    board_array.include? 5
+    board_array.include? 4
 
   end
 
